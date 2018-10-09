9c4ecf5c286cc381d5b398a96f07fe57a0c27048
WestleyK <38078774+WestleyK@users.noreply.github.com>
2018-10-09 14:13:26 -0700

    Create hello-world.jl

---
+++ lang/julia/hello-world.jl
@@ -0,0 +1,2 @@
+println("Hello world from: https://raw.githubusercontent.com/sylabs/examples/master/lang/julia/hello-world.jl")
+println("For full tutorial, visit: https://github.com/sylabs/examples/lang/julia")
