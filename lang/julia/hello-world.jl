0409169e0b30681e04dafde4f34829c0145b211f
WestleyK <38078774+WestleyK@users.noreply.github.com>
2018-10-10 11:58:03 -0700

    Added: env julia

--- lang/julia/hello-world.jl
+++ lang/julia/hello-world.jl
@@ -1,2 +1,4 @@
+#!/usr/bin/env julia
+
 println("Hello world from: https://raw.githubusercontent.com/sylabs/examples/master/lang/julia/hello-world.jl")
 println("For full tutorial, visit: https://github.com/sylabs/examples/lang/julia")
